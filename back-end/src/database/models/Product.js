f4f205a27218869ae409a7357404d412dde2f289
Willian Dutra <contatowilliandutra@gmail.com>
2023-05-02 17:52:22 -0300

    feat: Criando model Product

---
+++ back-end/src/database/models/Product.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define(
+    'Product', 
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      name: DataTypes.STRING,
+      price: DataTypes.DECIMAL,
+      urlImage: DataTypes.STRING,
+    },
+    { timestamps: false, underscored: true, tableName: 'products' },
+  );
+
+  Product.associate = (models) => {
+    Product.hasMany(models.User, { foreignKey: 'id', as: 'sale_product' });
+  };
+
+  return Product;
+};
--- back-end/src/database/models/Sale.js
+++ back-end/src/database/models/Sale.js
@@ -1,16 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
   const Sale = sequelize.define(
-  'Sale', 
-  { id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    userId: DataTypes.INTEGER,
-    sellerId: DataTypes.INTEGER,
-    totalPrice: DataTypes.DECIMAL,
-    deliveryAdrress: DataTypes.STRING,
-    deliveryNumber: DataTypes.STRING,
-    saleData: DataTypes.DATE,
-    status: DataTypes.STRING },
-  { timestamps: false, underscored: true, tableName: 'sales' },
-);
+    'Sale', 
+    { id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      userId: DataTypes.INTEGER,
+      sellerId: DataTypes.INTEGER,
+      totalPrice: DataTypes.DECIMAL,
+      deliveryAdrress: DataTypes.STRING,
+      deliveryNumber: DataTypes.STRING,
+      saleData: DataTypes.DATE,
+      status: DataTypes.STRING },
+    { timestamps: false, underscored: true, tableName: 'sales' },
+  );
 
   Sale.associate = (models) => {
     Sale.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
