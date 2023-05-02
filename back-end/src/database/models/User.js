8f6a62e369e67c93621d70e44bd1963b81de3005
Willian Dutra <contatowilliandutra@gmail.com>
2023-05-02 17:38:29 -0300

    feat: Criando model User

---
+++ back-end/src/database/models/User.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, primarKey: true },
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    { timestamps: false, tableName: 'users' },
+  );
+
+  User.associate = (models) => {
+    User.hasMany(models.Sale, { foreignKey: 'user_id', as: 'sale' });
+    User.hasMany(models.Sale, { foreignKey: 'seller_id', as: 'sale' });
+  };
+
+  return User;
+};
