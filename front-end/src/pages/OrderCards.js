81a44050d7e0fe96852d4c6d2162b89eae37428b
Willian Dutra <contatowilliandutra@gmail.com>
2023-05-23 19:52:35 +0000

    style: Estilizando pagina de Orders do Seller

--- front-end/src/pages/OrderCards.js
+++ front-end/src/pages/OrderCards.js
@@ -27,30 +27,61 @@ export default function OrderCards() {
   return (
     <>
       <NavSeller />
-      <div>
+      <main className=" w-4/5 mx-auto mt-6">
         { dataResult.map((ele, index) => (
           <Link
             key={ index }
             to={ `/seller/orders/${ele.id}` }
+            className={ `w-4/5 mx-auto border shadow-md rounded m-2 
+              flex flex-wrap bg-lightgray h-32 mb-4 justify-between` }
           >
-            <p data-testid={ `seller_orders__element-order-id-${ele.id}` }>
-              {`PEDIDO ${ele.id?.toString().padStart(four, '0')}`}
+            <p
+              className="bg-white h-full w-1/4 text-center pt-10 font-light rounded-l"
+              data-testid={ `seller_orders__element-order-id-${ele.id}` }
+            >
+              PEDIDO
+              <span
+                className="block text-center font-normal"
+              >
+                { (ele.id)?.toString().padStart(four, '0') }
+              </span>
             </p>
-            <p data-testid={ `seller_orders__element-delivery-status-${ele.id}` }>
+            <p
+              data-testid={ `seller_orders__element-delivery-status-${ele.id}` }
+              className={ `m-2 h-20 rounded flex w-1/3 justify-center
+                items-center font-semibold uppercase text-xl
+                ${ele.status === 'Entregue' ? 'bg-darkgreen' : ''}
+                ${ele.status === 'Pendente' ? 'bg-yellow-300' : ''}
+                ${ele.status === 'Preparando' ? 'bg-lime-400' : ''}
+                ${ele.status === 'Em Trânsito' ? 'bg-blue' : ''}` }
+            >
               {ele.status}
             </p>
-            <p data-testid={ `seller_orders__element-order-date-${ele.id}` }>
-              { formateDate(ele.saleDate)}
-            </p>
-            <p data-testid={ `seller_orders__element-card-price-${ele.id}` }>
-              { ele.totalPrice }
-            </p>
-            <p data-testid={ `seller_orders__element-card-address-${ele.id}` }>
+            <div className="flex-wrap w-2/6 mt-2 font-semibold text-xl mr-2">
+              <p
+                data-testid={ `seller_orders__element-order-date-${ele.id}` }
+                className="bg-white mb-2 h-9 rounded flex justify-center items-center"
+              >
+                { formateDate(ele.saleDate)}
+              </p>
+              <p
+                className="bg-white h-9 rounded flex justify-center items-center"
+              >
+                {'R$ '}
+                <span data-testid={ `seller_orders__element-card-price-${ele.id}` }>
+                  { ele.totalPrice.toString().replace('.', ',') }
+                </span>
+              </p>
+            </div>
+            <p
+              data-testid={ `seller_orders__element-card-address-${ele.id}` }
+              className="ml-auto -mt-8 mr-3"
+            >
               { `${ele.deliveryAddress}, ${ele.deliveryNumber}` }
             </p>
           </Link>
         ))}
-      </div>
+      </main>
     </>
   );
 }
